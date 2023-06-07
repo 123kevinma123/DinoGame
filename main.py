f0da8875b156b70d7c3c74ffcb22307ec8d2caaf
123kevinma123 <123kevinma123@gmail.com>
2023-06-06 17:38:29 -0700

    jumping animation

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 import pygame
 
 pygame.init()
-WIDTH, HEIGHT = 900, 500
-x, y = 435, 235
 black = (0, 0, 0)
 white = (255, 255, 255)
 red = (255, 0, 0)
+fall_speed = 2
+jump_speed = 100
 
-WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+WIN = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 pygame.display.set_caption("Flappy Bird")
 WIN.fill(black)
+clock=pygame.time.Clock()
+info_obj = pygame.display.Info()
+x, y = info_obj.current_h / 2 , info_obj.current_w / 2
 
-
+def update():
+    global y
+    if (y + fall_speed < info_obj.current_h - 30):
+        y += fall_speed
+    pygame.draw.rect(WIN, red, pygame.Rect(x, y, 30, 30))
 def main():
     run = True
     global y
@@ -20,15 +27,15 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE and y - 25 > 0:
-                    y -= 25
-            #y += 10
+                if event.key == pygame.K_SPACE and y - jump_speed > 0:
+                    y -= jump_speed
+            
 
-            WIN.fill(black)
-            pygame.draw.rect(WIN, red, pygame.Rect(x, y, 30, 30))
-            pygame.display.update()
+        WIN.fill(black)
+        update()
+        pygame.display.flip()
+        clock.tick(60)
     pygame.quit()
 
 
